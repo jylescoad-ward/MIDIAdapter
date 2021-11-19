ad9cc015090ef6895d271abcf86b92bce4efd02c
Jyles Coad-Ward <jyles@dariox.club>
2021-11-19 19:41:48 +0800

    Support for OSS Used

--- src/index.js
+++ src/index.js
@@ -82,3 +82,30 @@ $("button[action=export]").on("click",async () =>
 	if (!location.canceled)
 		fs.writeFile(location.filePath, JSON.stringify(localStorage,null,"\t"),console.log);
 });
+
+var allPackages = [];
+var devpackages = [];
+Object.entries(require("./../package.json").dependencies).forEach(d => allPackages.push({name:d[0].replace("@",""),version:d[1]}));
+Object.entries(require("./../package.json").devDependencies).forEach(d => devpackages.push({name:d[0].replace("@",""),version:d[1]}));
+console.log(allPackages,devpackages)
+allPackages.forEach((package) => {
+	$("[action=OSS_Credits]>ul").append(`
+	<li>
+		<span type="name"><a href="#" class="external-link" location="https://npmjs.com/package/${package.name}">${package.name}</a></span>
+		<span type="version">${package.version}</span>
+	</li>`);
+});
+devpackages.forEach((package) => {
+	$("[action=OSS_Credits_Dev]>ul").append(`
+	<li>
+		<span type="name"><a href="#" class="external-link" location="https://npmjs.com/package/${package.name}">${package.name}</a></span>
+		<span type="version">${package.version}</span>
+	</li>`);
+});
+$("#about[role=settingsModalTab]>h4").append(" "+require("./../package.json").version);
+
+
+$("a.external-link").on('click',(me) =>
+{
+	require('electron').shell.openExternal(me.delegateTarget.attributes.location.value);
+});
